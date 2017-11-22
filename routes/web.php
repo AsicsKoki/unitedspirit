35f020b7c65fba02eee1a118c77d77e57241b032
Josif Kostić <joskekostic@gmail.com>
2017-11-22 20:24:16 +0100

    -This commit will add User and Admin authentication,registration,login backend

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'UserController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'AdminAuth\LoginController@getAdminLogin')->name('getAdminlogin');
+  Route::post('/login', 'AdminAuth\LoginController@postAdminLogin')->name('postAdminLogin');
+  Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'AdminAuth\RegisterController@getAdminRegister')->name('getAdminRegister');
+  Route::post('/register', 'AdminAuth\RegisterController@postAdminRegister')->name('postAdminRegister');
+
+  Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+});
+
+
+// Authentication Routes...
+Route::get('userLogin', 'Auth\LoginController@getUserLogin')->name('getUserLogin');
+Route::post('userLogin', 'Auth\LoginController@postUserLogin')->name('postUserLogin');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('userRegister', 'Auth\RegisterController@getUserRegister')->name('getUserRegister');
+Route::post('userRegister', 'Auth\RegisterController@postUserRegister')->name('postUserRegister');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
