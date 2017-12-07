ea66a4a6988622c9875107cdc48434f8691aa3ed
arsa <arsenije018@gmail.com>
2017-12-07 15:56:59 +0100

    Peglanje CSS/HTML

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <div class="navbar-wrapper">
 
         <nav class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
+            <div class="container cf">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                     <span class="sr-only">Toggle navigation</span>
@@ -10,8 +10,9 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                    <a class="navbar-brand" href="#">United Spirit</a>
-                    <img class="navbar-image" src="http://www.freeiconspng.com/uploads/res-light-purple-flowers-png-by-hanabell1-d6l6mwr-png-16.png" alt="">
+                    <a class="navbar-brand" href="#">
+                        <img src="img/logo.png" alt="self realization">
+                    </a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav navbar-right">
