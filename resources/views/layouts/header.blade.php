2effba75eb71d463027c68245d651900577c9f2d
johnob3 <john_ob3@yahoo.com>
2017-12-18 14:33:50 +0100

    translate header to french

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,16 +19,16 @@
                     
                         <li class="{{ Request::is('home') ? 'active' : '' }}"><a href="{{ URL::to('/') }}">Home</a></li>
                         @if(Request::is('home'))
-                        <li><a href="#experts">Our Experts</a></li>
-                        <li><a href="#donations">Donate</a></li>
+                        <li><a href="#experts">Nos experts</a></li>
+                        <li><a href="#donations">Faire un don</a></li>
                         <li><a href="#contact">Contact</a></li>
-                        <li><a href="{{ route('getSubscriptions') }}">Subscriptions</a></li>
+                        <li><a href="{{ route('getSubscriptions') }}">Inscription</a></li>
                         <li><a href="{{ route('getMyCampus') }}">Campus Box</a></li>
                         @else
-                        <li><a href="{{ URL::to('/home') }}#experts">Our Experts</a></li>
-                        <li><a href="{{ URL::to('/home') }}#donations">Donate</a></li>
+                        <li><a href="{{ URL::to('/home') }}#experts">Nos experts</a></li>
+                        <li><a href="{{ URL::to('/home') }}#donations">Faire un don</a></li>
                         <li><a href="{{ URL::to('/home') }}#contact">Contact</a></li>
-                        <li class="{{ Request::is('subscriptions') ? 'active' : '' }}"><a href="{{ route('getSubscriptions') }}">Subscriptions</a></li>
+                        <li class="{{ Request::is('subscriptions') ? 'active' : '' }}"><a href="{{ route('getSubscriptions') }}">Inscription</a></li>
                         <li class="{{ Request::is('mycampus') ? 'active' : '' }}"><a href="{{ route('getMyCampus') }}">Campus Box</a></li>
                         @endif
                         <!-- <li class="active"><a href="{{ URL::to('/')}}">Home</a></li>
@@ -42,8 +42,8 @@
                         @endif -->
                         <!-- Authentication Links -->
                         @guest
-                        <li class="{{ Request::is('userLogin') ? 'active' : '' }}"><a href="{{ route('getUserLogin') }}">Login</a></li>
-                        <li class="{{ Request::is('userRegister') ? 'active' : '' }}"><a href="{{ route('getUserRegister') }}">Register</a></li>
+                        <li class="{{ Request::is('userLogin') ? 'active' : '' }}"><a href="{{ route('getUserLogin') }}">S'identifier</a></li>
+                        <li class="{{ Request::is('userRegister') ? 'active' : '' }}"><a href="{{ route('getUserRegister') }}">Registre</a></li>
                         @else
                             <li class="dropdown">
                                 <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -52,12 +52,12 @@
 
                                 <ul class="dropdown-menu" role="menu">
 
-                                    <li><a href="{{ route('getChangePassword') }}">Change password</a></li>
+                                    <li><a href="{{ route('getChangePassword') }}">Changer le mot de passe</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                                     Connectez - Out
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
